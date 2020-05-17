7066cd456b04849512ee5388f249498596c15376
candidomtz <candidomtz@me.com>
2020-05-16 22:59:41 -0500

    Create 004-calculadora.R

---
+++ scripts/tema1/004-calculadora.R
@@ -0,0 +1,18 @@
+2*(3+5/2)
+
+2*((3+5)/2)
+
+2%/%3+5
+#2 entre 3 es 0, +5 = 5
+2%%3
+#el resto de 2/3 es 2, porque en realidad es 0, pero para que dé 2, es 2
+2^3*5
+
+2*pi
+
+choose(2,1)
+
+choose(5, 3) #vamos a comprobarlo con la fórmula que dijo en clase, pero utilizando factoriales de la siguiente manera:
+factorial(5)/(factorial(3)*factorial(5-3)) #Sí sale! jajaja
+
+cos(90*pi/180)
